d843d3fb53ac866e3324721461b765c93e7dd150
yukis1996 <58467980+yukis1996@users.noreply.github.com>
2020-06-15 18:36:06 +0900

    migration create

---
+++ src/main/resources/db/migration/V2__create_tasks_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `tasks` (
+
+	    `task_id`    int          NOT NULL AUTO_INCREMENT,
+		`task_name`  varchar(255) NOT NULL,
+		`day`        date         NOT NULL,
+		`prioritize` int          NOT NULL DEFAULT 1,
+		`status`     int          NOT NULL DEFAULT 1,
+		`user_id`    int          NOT NULL ,
+		PRIMARY KEY (`task_id`),
+		FOREIGN KEY (`user_id`)   REFERENCES `users` (`user_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
