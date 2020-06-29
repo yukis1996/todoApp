e211f3224c3cfa0075e896cc1e6e7ffa13a25e1d
yukis1996 <58467980+yukis1996@users.noreply.github.com>
2020-06-29 12:41:25 +0900

    a

--- src/main/java/jp/co/aivick/todoapp/controller/LootController.java
+++ src/main/java/jp/co/aivick/todoapp/controller/LootController.java
@@ -1,14 +1,14 @@
-package jp.co.aivick.todoapp.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/")
-public class LootController {
-
-	@RequestMapping
-	public String showLoginForm() {
-		return "/login";
-	}
-}
+//package jp.co.aivick.todoapp.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/")
+//public class LootController {
+//
+//	@RequestMapping
+//	public String showLoginForm() {
+//		return "/login";
+//	}
+//}
